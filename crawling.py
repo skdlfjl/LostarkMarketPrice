a48a1dfc6f4cf95f13c32f8dababf02426fd33f5
skdlfjl <ok951108@gmail.com>
2021-10-13 19:02:50 +0900

    크롤링 데이터 전처리, 리스트 정리

--- crawling.py
+++ crawling.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from bs4 import BeautifulSoup
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome import options
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.select import Select
@@ -14,6 +15,34 @@ item = 11                 # 장신구 (11 = 목걸이 (특성2개))
 eff = [[49, 5], [3, 3]]   # [[각인1, 활성도], [각인2, 활성도]]
 cha = [2, 3]              # [특성1, 특성2]
 
+#---------------------------------------------------------
+# 크롤링 한 데이터에서 text만 뽑아내는 함수 (+ 공백과 줄바꿈 없애줌) 
+def crawling_text(in_list, out_list):
+        for i in in_list:
+            out_list.append(i.text.replace(" ","").replace("\n",""))
+
+# {'각성' : 6, '원한' : 3, '공격속도감소' : 1, '치명' : 403, '특화' : 446} 형태로 바꿔주는 함수 (!!!!!특성이 2개인 목걸이 전용!!!!!!)
+def effect_dict(effect_all):
+    for i in range(len(effect_all)):
+        effect_all[i] = effect_all[i].replace('[','').replace('활성도','').split(']+')
+
+    effect_list = []
+    for i in range(len(effect_all)):
+        if i % 5 == 0:
+            #print(effect_all[i:i+5])
+            effect_list.append(effect_all[i:i+5])
+            
+    dict_list = []
+    for i in effect_list:
+        dic = { i[0][0] : int(i[0][1]), i[1][0] : int(i[1][1]), i[2][0] : int(i[2][1]), i[3][0] : int(i[3][1]), i[4][0] : int(i[4][1])}
+        dict_list.append(dic)
+
+    #print(dict_list)
+    return dict_list
+
+#---------------------------------------------------------
+
+# 해당 함수 실행시 로그인하여 로스트아크 경매장 홈페이지에 들어가집니다 (최초 1번만 실행)
 def enter():
     #chrome_options = Options()
     #chrome_options.add_argument('--headlss')  # headless는 직접 크롬창에서 셀레니움이 어떻게 작동되는지 굳이 창을 열지 않고 싶을 때 이용.
@@ -27,7 +56,7 @@ def enter():
     login_x_path = '//*[@id="idLogin"]/div[3]/button'
     driver.find_element_by_name('user_id').send_keys('okc951108')
     driver.find_element_by_name('user_pwd').send_keys('fhtmxmdkzm1234')
-    driver.find_element_by_xpath(login_x_path).click()
+    driver.find_element_by_xpath(login_x_path).send_keys(Keys.ENTER)
 
     time.sleep(1)
     auction_url = "https://lostark.game.onstove.com/Auction"
@@ -35,17 +64,17 @@ def enter():
 
     return driver
 
-    # 로그인하여 로스트아크 경매장 홈페이지에 들어가집니다
 
-
+# 상세 옵션을 선택하여 검색 뒤, 크롤링 + 전처리 해줍니다 (input = 크롬드라이버, 장신구 종류, [각인, 활성도]*2, 특성*2)
 def main(driver, item, eff, cha):
     # 상세 옵션 검색버튼 클릭
     detail_option_x_path = '//*[@id="lostark-wrapper"]/div/main/div/div[3]/div[2]/form/fieldset/div/div[5]/button[2]'
-    driver.find_element_by_xpath(detail_option_x_path).click()  # 상세 옵션 검색 클릭
+    #driver.find_element_by_xpath(detail_option_x_path).click()  # 상세 옵션 검색 클릭
+    driver.find_element_by_xpath(detail_option_x_path).send_keys(Keys.ENTER)
 
     # 장신구 선택
     category_x_path = '//*[@id="selCategoryDetail"]/div[1]'
-    driver.find_element_by_xpath(category_x_path).click()  # 카테고리 클릭
+    driver.find_element_by_xpath(category_x_path).send_keys(Keys.ENTER)  # 카테고리 클릭
     driver.find_element_by_xpath('//*[@id="selCategoryDetail"]/div[2]/label[{}]'.format(item)).click()
     # //*[@id="selCategoryDetail"]/div[2]/label[10] : 전체
     # //*[@id="selCategoryDetail"]/div[2]/label[11] : 목걸이
@@ -54,8 +83,8 @@ def main(driver, item, eff, cha):
 
     # 아이템 등급 선택
     class_x_path = '//*[@id="modal-deal-option"]/div/div/div[1]/div[1]/table/tbody/tr[3]/td[1]/div/div[1]'
-    driver.find_element_by_xpath(class_x_path).click()  # 아이템 등급 클릭
-    driver.find_element_by_xpath('//*[@id="modal-deal-option"]/div/div/div[1]/div[1]/table/tbody/tr[3]/td[1]/div/div[2]/label[1]').click()
+    driver.find_element_by_xpath(class_x_path).send_keys(Keys.ENTER)  # 아이템 등급 클릭
+    driver.find_element_by_xpath('//*[@id="modal-deal-option"]/div/div/div[1]/div[1]/table/tbody/tr[3]/td[1]/div/div[2]/label[8]').click()
     # 전체 : 1 / 일반 : 2 / 고급 : 3 / 희귀 : 4 / 영웅 : 5 / 전설 : 6
     # //*[@id="modal-deal-option"]/div/div/div[1]/div[1]/table/tbody/tr[3]/td[1]/div/div[2]/label[7] : 유물
     # //*[@id="modal-deal-option"]/div/div/div[1]/div[1]/table/tbody/tr[3]/td[1]/div/div[2]/label[8] : 고대
@@ -63,21 +92,21 @@ def main(driver, item, eff, cha):
 
     # 각인 선택1 + 활성값 입력
     option1_x_path = '//*[@id="selEtc_0"]/div[1]' 
-    driver.find_element_by_xpath(option1_x_path).click()  # 기타 선택1 클릭
+    driver.find_element_by_xpath(option1_x_path).send_keys(Keys.ENTER)  # 기타 선택1 클릭
     driver.find_element_by_xpath('//*[@id="selEtc_0"]/div[2]/label[3]').click()  # 각인 효과 클릭
     option1Sub_x_path = '//*[@id="selEtcSub_0"]/div[1]' 
-    driver.find_element_by_xpath(option1Sub_x_path).click()  # 옵션 선택 클릭
-    driver.find_element_by_xpath('//*[@id="selEtcSub_{0}"]/div[2]/label[{1}]'.format(0, eff[0][0])).click()  # 4 (강령술) 각인 선택
+    driver.find_element_by_xpath(option1Sub_x_path).send_keys(Keys.ENTER)  # 옵션 선택 클릭
+    driver.find_element_by_xpath('//*[@id="selEtcSub_0"]/div[2]/label[{}]'.format(eff[0][0])).click()  # 4 (강령술) 각인 선택
     driver.find_element_by_id('txtEtcMin_0').send_keys(str(eff[0][1]))  # 위 각인의 활성값이 6인것만 출력
     driver.find_element_by_id('txtEtcMax_0').send_keys(str(eff[0][1]))
 
     # 각인 선택2 + 활성값 입력
     option2_x_path= '//*[@id="selEtc_1"]/div[1]'
-    driver.find_element_by_xpath(option2_x_path).click()  # 기타 선택2 클릭
+    driver.find_element_by_xpath(option2_x_path).send_keys(Keys.ENTER)  # 기타 선택2 클릭
     driver.find_element_by_xpath('//*[@id="selEtc_1"]/div[2]/label[3]').click()  # 각인 효과 클릭
     option2Sub_x_path = '//*[@id="selEtcSub_1"]/div[1]' 
-    driver.find_element_by_xpath(option2Sub_x_path).click()  # 옵션 선택 클릭
-    driver.find_element_by_xpath('//*[@id="selEtcSub_{0}"]/div[2]/label[{1}]'.format(1, eff[1][0])).click()  # 53 (원한) 각인 선택
+    driver.find_element_by_xpath(option2Sub_x_path).send_keys(Keys.ENTER)  # 옵션 선택 클릭
+    driver.find_element_by_xpath('//*[@id="selEtcSub_1"]/div[2]/label[{}]'.format(eff[1][0])).click()  # 53 (원한) 각인 선택
     driver.find_element_by_id('txtEtcMin_1').send_keys(str(eff[1][1]))  # 위 각인의 활성값이 3인것만 출력
     driver.find_element_by_id('txtEtcMax_1').send_keys(str(eff[1][1]))
 
@@ -90,23 +119,23 @@ def main(driver, item, eff, cha):
 
     # 특성 선택1
     option3_x_path= '//*[@id="selEtc_2"]/div[1]'
-    driver.find_element_by_xpath(option3_x_path).click()  # 기타 선택3 클릭
+    driver.find_element_by_xpath(option3_x_path).send_keys(Keys.ENTER)  # 기타 선택3 클릭
     driver.find_element_by_xpath('//*[@id="selEtc_2"]/div[2]/label[2]').click()  # 전투 특성 클릭
     option3Sub_x_path = '//*[@id="selEtcSub_2"]/div[1]' 
-    driver.find_element_by_xpath(option3Sub_x_path).click()  # 옵션 선택 클릭
-    driver.find_element_by_xpath('//*[@id="selEtcSub_{0}"]/div[2]/label[{1}]'.format(2, cha[0])).click()  # 2 (치명) 특성 선택
+    driver.find_element_by_xpath(option3Sub_x_path).send_keys(Keys.ENTER)  # 옵션 선택 클릭
+    driver.find_element_by_xpath('//*[@id="selEtcSub_2"]/div[2]/label[{}]'.format(cha[0])).click()  # 2 (치명) 특성 선택
     # 특성 선택2
     option4_x_path= '//*[@id="selEtc_3"]/div[1]'
-    driver.find_element_by_xpath(option4_x_path).click()  # 기타 선택4 클릭
+    driver.find_element_by_xpath(option4_x_path).send_keys(Keys.ENTER)  # 기타 선택4 클릭
     driver.find_element_by_xpath('//*[@id="selEtc_3"]/div[2]/label[2]').click()  # 전투 특성 클릭
     option4Sub_x_path = '//*[@id="selEtcSub_3"]/div[1]' 
-    driver.find_element_by_xpath(option4Sub_x_path).click()  # 옵션 선택 클릭
-    driver.find_element_by_xpath('//*[@id="selEtcSub_{0}"]/div[2]/label[{1}]'.format(3, cha[1])).click()  # 3 (특화) 특성 선택
+    driver.find_element_by_xpath(option4Sub_x_path).send_keys(Keys.ENTER)  # 옵션 선택 클릭
+    driver.find_element_by_xpath('//*[@id="selEtcSub_3"]/div[2]/label[{}]'.format(cha[1])).click()  # 3 (특화) 특성 선택
     # label[2] = 치명 / label[3] = 특화
 
 
     search_x_path = '//*[@id="modal-deal-option"]/div/div/div[2]/button[1]'
-    driver.find_element_by_xpath(search_x_path).click()   # 검색버튼 클릭 (여기까지 총 13초) >> 검색 자체만으로 걸리는 시간은 4초
+    driver.find_element_by_xpath(search_x_path).send_keys(Keys.ENTER)   # 검색버튼 클릭 (여기까지 총 13초) >> 검색 자체만으로 걸리는 시간은 4초
 
 
     ### 크롤링 해오기 ###
@@ -115,15 +144,23 @@ def main(driver, item, eff, cha):
     soup = BeautifulSoup(html, 'html.parser') # BeautifulSoup사용하기
     name = soup.select('span.name')
     effect = soup.select('div.effect > ul > li')
-    price_buy = soup.select('div.price-buy')
+    price = soup.select('div.price-buy')
 
-    def crawling(in_list):
-        for i in in_list:
-            print(i.text.replace(" ","").replace("\n",""))
+    name_list = []
+    effect_all = []  
+    price_list = []        
+    crawling_text(name, name_list)
+    crawling_text(effect, effect_all)      # ['[각성]활성도+6', '[원한]활성도+3', '[공격속도감소]활성도+1', ... ]  >> 전처리 필요함!!
+    effect_list = effect_dict(effect_all)  # {'각성' : 6, '원한' : 3, '공격속도감소' : 1, '치명' : 403, '특화' : 446} >> 전처리 완료
+    crawling_text(price, price_list)
 
-    crawling(name)
-    crawling(effect)
-    crawling(price_buy)
+    item_list = []
+    for i in range(len(name_list)):
+        item_list.append([name_list[i], effect_list[i], price_list[i]])
+    
+    return item_list
+
+
 
 
 
@@ -132,7 +169,7 @@ def main(driver, item, eff, cha):
     x_index = 3
     while x_index < 13:
         time.sleep(1.2)   # element is not attached to the page document 에러 해결
-        driver.find_element_by_xpath(f'//*[@id="auctionList"]/div[2]/a[{x_index}]').click()
+        driver.find_element_by_xpath(f'//*[@id="auctionList"]/div[2]/a[{x_index}]').send_keys(Keys.ENTER)
         
         # 해당 페이지 크롤링 코드 추가해야됨
 
