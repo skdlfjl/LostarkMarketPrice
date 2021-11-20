fedda420290a576e9a9763a3e204a3783c94c416
skdlfjl <ok951108@gmail.com>
2021-11-20 15:04:33 +0900

    combination으로 변경

--- all_combin.py
+++ all_combin.py
@@ -1,4 +1,4 @@
-from itertools import combinations, permutations, product
+from itertools import combinations, combinations_with_replacement, permutations, product
 import operator
 import numpy as np
 
@@ -46,6 +46,7 @@ def main(ability, ability_lv, item_act):
         # 레벨의 합이 16이고, 보물+각인서 합이 40 이상인 경우 43, 53 (333331)
         if sum(ability_lv) < 16 or (sum(ability_lv) == 16 and 40 <= sum(item_act)):
             item_act_min = sum(ability_lv_act) - sum(act_53)*5 
+            # 3, 4, 5로 만들 수 없는 1, 2를 3으로 치환
             if 1 in ability_act: ability_act[ability_act.index(1)] = 3
             if 2 in ability_act: ability_act[ability_act.index(2)] = 3
 
@@ -54,12 +55,9 @@ def main(ability, ability_lv, item_act):
         # 따라서 값을 치환해주지 않아도 된다. 값이 1, 2가 나와도 그냥 그 이상인 값이 들어갈테니!
 
         # 레벨의 합이 16이고, 보물+각인서 합이 35이상 39이하인 경우 53, 63 (333331)
+        # 레벨의 합이 17이고, 보물+각인서 합이 40이상인 경우 53, 63 (333332) / 40일때는 63 장신구만
         # 필요한 돌과 각인서의 최소값 = 장신구 5개의 최대 능력치(63) 
-        elif sum(ability_lv) == 16 and 35 <= sum(item_act) <= 39:
-            item_act_min = sum(ability_lv_act) - sum(act_63)*5 
-
-        # 레벨의 합이 17이고, 보물+각인서 합이 40이상인 경우 63 (333332)
-        elif sum(ability_lv) == 17 and 40 <= sum(item_act):
+        elif (sum(ability_lv) == 17 and 40 <= sum(item_act)) or (sum(ability_lv) == 16 and 35 <= sum(item_act) <= 39):
             item_act_min = sum(ability_lv_act) - sum(act_63)*5 
 
     # 만약 최소한으로 필요한 보물+각인서 활성값을 맞추지 못했거나
@@ -68,6 +66,8 @@ def main(ability, ability_lv, item_act):
 
 
     if item_act_min <= sum(item_act):
+        combi = list(permutations(act_33 + [0]*(len(ability)-2), len(ability)))  # 각인 4개면 [3, 3, 0, 0]
+        combi_33 = list(set(combi))
         combi = list(permutations(act_43 + [0]*(len(ability)-2), len(ability)))
         combi_43 = list(set(combi))
         combi = list(permutations(act_53 + [0]*(len(ability)-2), len(ability)))
@@ -75,30 +75,37 @@ def main(ability, ability_lv, item_act):
         combi = list(permutations(act_63 + [0]*(len(ability)-2), len(ability)))
         combi_63 = list(set(combi))
 
-        # 각인이 4~5개인 경우 33, 43, 53
-        if 4 <= len(ability) <= 5:
-            combi = list(permutations(act_33 + [0]*(len(ability)-2), len(ability)))  # 각인 4개면 [3, 3, 0, 0]
-            combi_33 = list(set(combi))
-            all_combi = combi_33 + combi_43 + combi_53
-            print('각인이 4~5개인 경우 : 33, 43, 53')
-        
-        # 각인이 6개인데, 
-        elif len(ability) == 6:
-            # 레벨의 합이 16보다 작은경우 (333321이 최대) 43, 53
-            # 레벨의 합이 16이고, 보물+각인서 합이 40 이상인 경우 43, 53 (333331)
-            if sum(ability_lv) < 16 or (sum(ability_lv) == 16 and 40 <= sum(item_act)):
-                all_combi = combi_43 + combi_53
-                print('각인 6개 (최대 333321 or 333331 + item_act 40이상) : 43, 53')
+        # 사용자가 입력한 돌+각인서 활성도 값 합이 [ 필요 활성도 합 - (5+3)*5 ] 와 같을 경우
+        # 53 장신구로만 조합이 나오도록! (53장신구로만 조합해야 가능하기때문)  
+        if sum(item_act) == sum(ability_lv_act) - sum(act_53)*5:       
+            all_combi = combi_53
+            print('53장신구로만 조합가능 (장신구 활성도 합 40)')    
+        elif len(ability) == 6 and sum(item_act) == sum(ability_lv_act) - sum(act_63)*5: 
+            # 고대등급으로만 조합하는건 각인 6개부터!
+            all_combi = combi_63
+            print('63장신구로만 조합가능 (장신구 활성도 합 45)')  
 
-            # 레벨의 합이 16이고, 보물+각인서 합이 35이상 39이하인 경우 53, 63 (333331)
-            elif sum(ability_lv) == 16 and 35 <= sum(item_act) <= 39:
-                all_combi = combi_53 + combi_63
-                print('각인 6개 / 333331 / 35 <= (보물+각인서) <= 39 : 53, 63(고대)')
+        else:  # 만약 전부 아닐경우, 기존의 방식대로 작동하도록!!
+            # 각인이 4~5개인 경우 33, 43, 53
+            if 4 <= len(ability) <= 5:
+                all_combi = combi_33 + combi_43 + combi_53
+                print('각인이 4~5개인 경우 : 33, 43, 53')
+           
+            # 각인이 6개인데, 
+            elif len(ability) == 6:
+                # 레벨의 합이 16보다 작은경우 (333321이 최대) 43, 53
+                # 레벨의 합이 16이고, 보물+각인서 합이 40 이상인 경우 43, 53 (333331)
+                if sum(ability_lv) < 16 or (sum(ability_lv) == 16 and 40 <= sum(item_act)):
+                    all_combi = combi_43 + combi_53
+                    print('각인 6개 (최대 333321 or 333331 + item_act 40이상) : 43, 53')
 
-            # 레벨의 합이 17이고, 보물+각인서 합이 40이상인 경우 63 (333332)
-            elif sum(ability_lv) == 17 and 40 <= sum(item_act):
-                all_combi = combi_63
-                print('각인 6개 / 333332 / 40 <= (보물+각인서) : 63(고대)')
+                # 유물 + 고대
+                # 레벨의 합이 16이고, 보물+각인서 합이 35이상 39이하인 경우 53, 63 (333331)
+                # 레벨의 합이 17이고, 보물+각인서 합이 40이상인 경우 63 (333332)
+                elif (sum(ability_lv) <= 16 and 35 <= sum(item_act) <= 39) or (sum(ability_lv) == 17 and 40 < sum(item_act)):
+                    all_combi = combi_53 + combi_63
+                    if sum(ability_lv) == 16: print('각인 6개 / 333331 / 35 <= (보물+각인서) <= 39 : 53, 63(고대)')
+                    elif sum(ability_lv) == 17: print('각인 6개 / 333332 / 40 < (보물+각인서) : 53, 63(고대)')
 
         ## all_combi : 
         #       사용자가 원하는 활성도를 위해, 적용 가능한 각인의 활성도의 모든 조합
@@ -107,9 +114,20 @@ def main(ability, ability_lv, item_act):
         all_combi_index = []
         for i in range(len(all_combi)):
             all_combi_index.append(i)
-        shuffle_index = list(permutations(all_combi_index, 4)) # 인덱스를 섞어준다
+        #############################################################################################################################
+        #shuffle_index = list(permutations(all_combi_index, 4)) # 인덱스를 섞어준다
+        # 기존 코드
+
+        print('all_combi :', all_combi)
+        s = list(combinations_with_replacement(all_combi_index, 2))
+        shuffle_index = []
+        for i in s:
+            for j in s:
+                shuffle_index.append(i + j)
+
         # (0, 1, 2, 3) >> all_combi 리스트 안 0번째, 1번째, 2번째, 3번째 튜플의 인덱스
-        # 인덱스는 무조건 4개가 나와야한다 (그래야 장신구 5개의 조합을 맞추기 때문)
+        # 인덱스는 무조건 4개가 나와야한다 (그래야 아래 코드에서 목걸이까지 추가해 장신구 5개의 조합을 맞추기 때문)
+        #############################################################################################################################
 
         necklace = []
         possible_combin = []
